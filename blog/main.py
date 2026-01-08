b72995af4aaa1eff5fc90ed5b192ce69949ec3b5
shogun1988 <nokahvandenhoven@gmail.com>
2026-01-08 15:45:42 +0100

    get blog from database

--- blog/main.py
+++ blog/main.py
@@ -14,6 +14,7 @@ def get_db():
     finally:
         db.close
 
+# create a new blog
 @app.post('/blog')
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
@@ -21,3 +22,15 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+# get all blog
+@app.get('/blog')
+def all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+# get single blog
+@app.get('/blog/{id}')
+def show(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    return blog
